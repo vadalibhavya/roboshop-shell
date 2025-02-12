25746b9a8bd85b8515af4ff44f675819dcd72f76
avadaliDevOps <adityavittal92@gmail.com>
2025-02-13 01:58:30 +0530

    Update

--- shipping.sh
+++ shipping.sh
@@ -12,3 +12,8 @@ mv target/shipping-1.0.jar shipping.jar
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
+dnf install mysql -y
+mysql -h mysql-dev.doubtfree.online -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql-dev.doubtfree.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h mysql-dev.doubtfree.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+systemctl restart shipping
